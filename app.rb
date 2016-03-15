58da5f6679c9d4a490860bcfd31b7b42d4cdd88a
Junyuan Xue <junyuanxue.is@gmail.com>
2016-03-15 15:22:50 +0000

    somehow modified app.rb

--- app.rb
+++ app.rb
@@ -21,7 +21,7 @@ class Battle < Sinatra::Base
     @player_2_name = session[:player_2_name]
     erb :play
   end
-  
+
 
   # start the server if ruby file executed directly
   run! if app_file == $0
