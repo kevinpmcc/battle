5d3e804ede93fddc188348645451ee0cb65ecd5d
Patrick <p_donati@hotmail.com>
2016-03-16 15:00:33 +0000

    did things

--- app.rb
+++ app.rb
@@ -1,5 +1,6 @@
 require 'sinatra/base'
 require './lib/player'
+require './lib/game'
 
 class Battle < Sinatra::Base
   enable :sessions
@@ -9,8 +10,9 @@ class Battle < Sinatra::Base
   end
 
   post '/names' do
-    $player_1 = Player.new(params[:player_1_name])
-    $player_2 = Player.new(params[:player_2_name])
+    player_1 = Player.new(params[:player_1_name])
+    player_2 = Player.new(params[:player_2_name])
+    $game = Game.new(player_1, player_2)
     redirect '/play'
   end
 
@@ -19,7 +21,9 @@ class Battle < Sinatra::Base
   end
 
   get '/attack' do
-    "#{$player_2.name} was attacked"
+  #  @player_2 = $player_2
+    $game.attack($game.player_2)
+    "#{$game.player_2.name} was attacked"
   end
 
   # start the server if ruby file executed directly
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,13 +1,21 @@
 require 'game'
 
 describe Game do
-  subject(:player) { double(:Player) }
-  subject(:game) { described_class.new }
+  subject(:game) { described_class.new(player_1, player_2) }
+  let(:player_1) { double :player }
+  let(:player_2) { double :player }
+
+  describe '#initialize' do
+    it 'it initializes with two instances of the player class' do
+      expect(game.player_1).to eq player_1
+      expect(game.player_2).to eq player_2
+    end
+  end
 
   describe '#attack' do
     it 'execute an attack' do
-      expect(player).to receive(:receive_attack)
-      game.attack(player)
+      expect(player_1).to receive(:receive_attack)
+      game.attack(player_1)
     end
   end
 end
