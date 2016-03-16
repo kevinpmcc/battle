4b27b1e77ecd7d21b0532e22468456a274846820
Patrick <p_donati@hotmail.com>
2016-03-16 12:37:31 +0000

    recieve attack

--- lib/player.rb
+++ lib/player.rb
@@ -1,7 +1,13 @@
 class Player
-  attr_reader :name
+  attr_reader :name, :hp
+  DEFAULT_HP = 60
 
   def initialize(name)
     @name = name
+    @hp = DEFAULT_HP
+  end
+
+  def receive_attack
+    @hp -= 6
   end
 end
--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -3,6 +3,7 @@ require 'player'
 describe Player do
   subject(:player) { described_class.new(name) }
   let(:name) { double :String }
+  hp = Player::DEFAULT_HP
 
   describe '#name' do
     it 'will return the name' do
@@ -10,4 +11,15 @@ describe Player do
     end
   end
 
-end 
+  describe '#hp' do
+    it 'will return the hp' do
+      expect(player.hp).to eq hp
+    end
+  end
+
+  describe '#receive_attack' do
+    it 'will reduce the hp' do
+      expect{ player.receive_attack }.to change{ player.hp }.by(-6)
+    end
+  end
+end
