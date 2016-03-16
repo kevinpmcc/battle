83fec66c0f410b36c085b87a41d05a7fea0f25f9
Patrick <p_donati@hotmail.com>
2016-03-16 11:13:00 +0000

    refactoring

--- spec/features/hit_points_spec.rb
+++ spec/features/hit_points_spec.rb
@@ -2,12 +2,7 @@ require 'spec_helper'
 
 feature "display hit points" do
   scenario "users have hit points" do
-    visit "/"
-     
-    fill_in "player_1_name", :with => "ass"
-    fill_in "player_2_name", :with => "boob"
-    click_button "Submit"
-
+    sign_in_and_play
     expect(page).to have_content "boob: 60HP"
   end
 end
