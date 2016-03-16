ebbc579af43bb2839972299c3254e495c3c9cc28
kevinpmcc <kevinpatrickmccarthy@gmail.com>
2016-03-16 12:12:18 +0000

    Challenge 23: extracted logic to class Player

--- spec/features/attack_spec.rb
+++ spec/features/attack_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 feature 'attack' do
   scenario 'attacks player 2' do
     sign_in_and_play
-    click_button 'Attack Player 2'
+    click_button 'Attack boob'
     expect(page).to have_content 'boob was attacked'
   end
 end
